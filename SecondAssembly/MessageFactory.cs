b0f86660da5f2d2cbd2ce8b30a42802f2ae9ecb0
SergeyPC <tennirobo@gmail.com>
2024-05-14 20:03:42 +0200

    init

---
+++ SecondAssembly/MessageFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAssembly
+{
+    public class MessageFactory
+    {
+        public string GetMessage()
+        {
+            return "This is a message from the second assembly";
+        }
+    }
+}
